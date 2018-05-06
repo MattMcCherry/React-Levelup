ba17ffb5750e7d5fb0b69601f22ae35b10e4dd78
Matt McCherry <matt.mccherry@gmail.com>
2018-05-06 12:08:08 +0100

    movie component added

--- src/App.js
+++ src/App.js
@@ -2,43 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Spider Man'
+  },
+  {
+    id: 3,
+    title: 'The Greatest Showman'
+  },
+  {
+    id: 4,
+    title: 'The Lion King'
+  },
+];
+
 class App extends Component {
-
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (e) => {
-    this.setState({ input: e.target.value.trim() });
-  }
-
-  submit = () => {
-  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to React" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h3>{this.state.input}</h3>
-        <input type="text" onChange={this.updateInput} value={this.state.input} />
-        <input type="text" ref={(input) => this.text = input}/>
-        <button onClick={this.submit}>Show Value</button>
+        {movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
       </div>
-    );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text, toggle } = this.props;
-    return (
-      <h1 className="App-title">{text}</h1>
     );
   }
 }
