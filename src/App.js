2eab908407a09f9e1656c3dac6493dd49e4b143c
Matt McCherry <matt.mccherry@gmail.com>
2018-05-13 17:48:56 +0100

    Router imported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Movie from './movie';
 
@@ -11,9 +12,9 @@ class App extends Component {
 
   async componentDidMount() {
     try {
-      const url =
+      const URL_PATH =
         'https://api.themoviedb.org/3/discover/movie?api_key=d033296ae7302fbf5213060b2d508924';
-      const res = await fetch(url);
+      const res = await fetch(URL_PATH);
       const movies = await res.json();
       this.setState({
         movies: movies.results,
@@ -25,12 +26,14 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
+        </div>
+      </Router>
     );
   }
 }
