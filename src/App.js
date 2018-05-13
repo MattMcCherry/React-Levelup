a8dbe4471e65f35548efbadc83d3d840fc7e3f3c
Matt McCherry <matt.mccherry@gmail.com>
2018-05-13 17:24:41 +0100

    changed movie into function stateless component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './App.css';
 import Movie from './movie';
 
 class App extends Component {
-
   state = {
-    movies: []
-  }
+    movies: [],
+  };
 
   async componentDidMount() {
     try {
-      const url = "https://api.themoviedb.org/3/discover/movie?api_key=d033296ae7302fbf5213060b2d508924";
+      const url =
+        'https://api.themoviedb.org/3/discover/movie?api_key=d033296ae7302fbf5213060b2d508924';
       const res = await fetch(url);
       const movies = await res.json();
       this.setState({
-        movies: movies.results
-      })
-    } catch(e) {
+        movies: movies.results,
+      });
+    } catch (e) {
       console.log(e);
     }
   }
@@ -29,7 +29,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc}/>)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
       </div>
     );
   }
